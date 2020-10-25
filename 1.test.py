08ff4a669a4f1051e5abcad1eac21961e60153bc
sagarchilaka <chilaka.sagarbabu@gmail.com>
2020-10-25 21:28:36 +0530

    11 commit

--- 1.test.py
+++ 1.test.py
@@ -1,6 +1,1 @@
-this file is for checkng individual file checking
-chehcking the file for push and pull request
-
-whether it is correct or this one is correct we are checking 
-this is the second time we are checking whether the file is present or not
-
+Developer 1 
